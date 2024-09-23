8f7b1c25d2e622b0bbe954a2264e2e52ad26cb0a
Mario <mjcarrion@uamv.edu.ni>
2024-09-23 08:24:38 -0600

    Se agrego la clase Registro

---
+++ Corte1/Corte1/Registro.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Corte1
+{
+    internal class Registro
+    {
+        private List<Persona> personas;
+        private const int MaxPersonas = 31;
+
+        public Registro()
+        {
+            personas = new List<Persona>();
+        }
+
+        public void AgregarPersona(Persona persona)
+        {
+            if (personas.Count < MaxPersonas)
+                personas.Add(persona);
+            else
+                Console.WriteLine("No se puede agregar mas personas, el registro esta lleno.");
+        }
+
+        public List<Persona> ObtenerPersonas()
+        {
+            return personas;
+        }
+
+    }
+}
