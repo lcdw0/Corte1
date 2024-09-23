2da49a62a948c8a8790108255fbce752232b317a
Mario <mjcarrion@uamv.edu.ni>
2024-09-23 08:30:32 -0600

    Se agrego la clase Operacion, para cacular la edad

---
+++ Corte1/Corte1/Operacion.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Corte1
+{
+    internal class Operacion
+    {
+        public int CalcularEdad(Persona persona)
+        {
+            int edad = DateTime.Now.Year - persona.FechaNacimiento.Year;
+            if (DateTime.Now.DayOfYear < persona.FechaNacimiento.DayOfYear)
+                edad -= 1;
+            return edad;
+        }
+
+        public bool EsMayorDeEdad(Persona persona)
+        {
+            return CalcularEdad(persona) >= 18;
+        }
+
+    }
+}
